3f9c1a8f5f96faa5c6eaa7a34d49965c5d659832
datrell28 <54969523+datrell28@users.noreply.github.com>
2022-02-28 23:43:11 -0500

    BasicMovement
    Added basic movement

---
+++ N330GLCombat1/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementVector = new Vector2();
+
+        movementVector.x = Input.GetAxis("Horizontal") * speed;
+
+        bool grounded = Physics2D.OverlapCircle(groundCheck.position, .4f, groundLayer) != null;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            movementVector.y = jumpForce;
+        }
+
+        rb.AddForce(movementVector);
+    }
+}
