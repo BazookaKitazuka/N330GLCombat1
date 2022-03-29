7005370a34c9e01958a3bd14b2f289ae19a6064f
datrell28 <54969523+datrell28@users.noreply.github.com>
2022-03-28 23:13:50 -0400

    attack

--- N330GLCombat1/Assets/Scripts/PlayerAttack.cs
+++ N330GLCombat1/Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
